cf55b5c281e7963cbb63ffa47236a01fe9bf5182
Starbeamrainbowlabs <sbrl@starbeamrainbowlabs.com>
2020-09-09 17:14:06 +0100

    download: make sure that the output directory exists

--- src/subcommands/download/index.mjs
+++ src/subcommands/download/index.mjs
@@ -1,5 +1,7 @@
 "use strict";
 
+import fs from 'fs';
+
 import settings from '../../bootstrap/settings.mjs';
 import l from '../../helpers/Log.mjs';
 
@@ -40,19 +42,23 @@ export default async function() {
 	// Validate the other settings
 	if(typeof settings.config.output != "string" || settings.config.output == "CHANGE_ME") {
 		l.error(`Error: No output directory was specified in the config file.`);
-		process.exit(2);
+		process.exit(1);
+	}
+	if(!fs.existsSync(settings.config.output)) {
+		l.error(`Error: The output directory '${settings.config.output}' doesn't exist.`);
+		process.exit(3);
 	}
 	if(typeof settings.config.ftp.url != "string" || settings.config.ftp.url == "CHANGE_ME") {
 		l.error(`Error: No ftp url was specified in the config file.`);
-		process.exit(2);
+		process.exit(1);
 	}
 	if(typeof settings.config.ftp.username != "string" || settings.config.ftp.username == "CHANGE_ME") {
 		l.error(`Error: No ftp username was specified in the config file.`);
-		process.exit(2);
+		process.exit(1);
 	}
 	if(typeof settings.config.ftp.password != "string" || settings.config.ftp.password == "CHANGE_ME") {
 		l.error(`Error: No ftp password was specified in the config file.`);
-		process.exit(2);
+		process.exit(1);
 	}
 	if(settings.cli.blacklist_file instanceof Array) {
 		for(let filename of settings.cli.blacklist_file) {
