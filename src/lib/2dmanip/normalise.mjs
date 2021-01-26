91f7beca2b3335a244118e0a9b3b6ea0f0e7fff5
Starbeamrainbowlabs <sbrl@starbeamrainbowlabs.com>
2021-01-26 17:42:50 +0000

    normalise: a

--- src/lib/2dmanip/normalise.mjs
+++ src/lib/2dmanip/normalise.mjs
@@ -14,7 +14,7 @@
  * @return {void}               This MUTATES the original array!
  */
 export default function(arr, min, max, rescale_min, rescale_max, clamp = true, round = false) {
-	for(let y = arr.length; y > 0; y--) {
+	for(let y = arr.length - 1; y > 0; y--) {
 		for(let x = arr[y].length; x > 0; y--) {
 			if(clamp)
 				arr[y][x] = Math.max(min, Math.min(arr[y][x], max));
