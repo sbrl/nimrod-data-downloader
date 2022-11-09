2ed18363603ceb670b28c469b72f73adf9d609e5
Starbeamrainbowlabs <sbrl@starbeamrainbowlabs.com>
2022-11-09 16:47:06 +0000

    child_process/tar: properly remove event listeners to help garbage collector

--- src/lib/child_process/tar.mjs
+++ src/lib/child_process/tar.mjs
@@ -5,8 +5,16 @@ import child_process from 'child_process';
 function untar(archive, target) {
 	return new Promise((resolve, reject) => {
 		let tar_process = child_process.spawn("tar", [ "-xf", archive, "-C", target ]);
-		tar_process.on("exit", resolve);
-		tar_process.on("error", reject);
+		tar_process.on("exit", (...args) => {
+			tar_process.off("exit", resolve);
+			tar_process.off("error", reject);
+			resolve(...args);
+		});
+		tar_process.on("error", (...args) => {
+			tar_process.off("exit", resolve);
+			tar_process.off("error", reject);
+			reject(...args);
+		});
 	});
 }
 
